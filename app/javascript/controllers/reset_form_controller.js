4bae65730718d0fc6aba1d829b4313c20d9991f0
Florent <florent.vincerot@me.com>
2023-06-01 18:25:51 +0200

    play with turbo stream

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -1,6 +1,13 @@
-import { Controller } from "stimulus"
+import { Controller } from "@hotwired/stimulus"
 
+// Connects to data-controller="reset-form"
 export default class extends Controller {
+  connect() {
+    console.log("Hello from our first Stimulus controller")
+    this.element.textContent = "Hello hello reset form!"
+
+  }
+
   reset() {
     this.element.reset()
   }
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,5 @@
 class Room < ApplicationRecord
   has_many :articles
+  broadcasts
+  # after_create_commit -> { broadcast_append_to room }
 end
